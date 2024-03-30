8cf3b3ed69d6dafec7697a730e3a861956e9d62c
Akash <Akash@LAPTOP-AGD0GN4V>
2024-03-30 14:57:22 +0530

    updates30mar

--- seprojectreal/adminForm2.Designer.vb
+++ seprojectreal/adminForm2.Designer.vb
@@ -22,9 +22,451 @@ Partial Class adminForm2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "adminForm2"
+        ProgressBar1 = New ProgressBar()
+        ProgressBar2 = New ProgressBar()
+        ProgressBar3 = New ProgressBar()
+        ProgressBar4 = New ProgressBar()
+        ProgressBar5 = New ProgressBar()
+        ProgressBar6 = New ProgressBar()
+        ProgressBar7 = New ProgressBar()
+        ProgressBar8 = New ProgressBar()
+        ProgressBar9 = New ProgressBar()
+        ProgressBar10 = New ProgressBar()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        CheckBox5 = New CheckBox()
+        CheckBox6 = New CheckBox()
+        CheckBox7 = New CheckBox()
+        CheckBox8 = New CheckBox()
+        CheckBox9 = New CheckBox()
+        CheckBox10 = New CheckBox()
+        btnBack = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(88, 104)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(125, 29)
+        ProgressBar1.TabIndex = 0
+        ' 
+        ' ProgressBar2
+        ' 
+        ProgressBar2.Location = New Point(88, 162)
+        ProgressBar2.Name = "ProgressBar2"
+        ProgressBar2.Size = New Size(125, 29)
+        ProgressBar2.TabIndex = 1
+        ' 
+        ' ProgressBar3
+        ' 
+        ProgressBar3.Location = New Point(88, 220)
+        ProgressBar3.Name = "ProgressBar3"
+        ProgressBar3.Size = New Size(125, 29)
+        ProgressBar3.TabIndex = 2
+        ' 
+        ' ProgressBar4
+        ' 
+        ProgressBar4.Location = New Point(88, 271)
+        ProgressBar4.Name = "ProgressBar4"
+        ProgressBar4.Size = New Size(125, 29)
+        ProgressBar4.TabIndex = 3
+        ' 
+        ' ProgressBar5
+        ' 
+        ProgressBar5.Location = New Point(88, 334)
+        ProgressBar5.Name = "ProgressBar5"
+        ProgressBar5.Size = New Size(125, 29)
+        ProgressBar5.TabIndex = 4
+        ' 
+        ' ProgressBar6
+        ' 
+        ProgressBar6.Location = New Point(443, 271)
+        ProgressBar6.Name = "ProgressBar6"
+        ProgressBar6.Size = New Size(125, 29)
+        ProgressBar6.TabIndex = 5
+        ' 
+        ' ProgressBar7
+        ' 
+        ProgressBar7.Location = New Point(443, 220)
+        ProgressBar7.Name = "ProgressBar7"
+        ProgressBar7.Size = New Size(125, 29)
+        ProgressBar7.TabIndex = 6
+        ' 
+        ' ProgressBar8
+        ' 
+        ProgressBar8.Location = New Point(443, 162)
+        ProgressBar8.Name = "ProgressBar8"
+        ProgressBar8.Size = New Size(125, 29)
+        ProgressBar8.TabIndex = 7
+        ' 
+        ' ProgressBar9
+        ' 
+        ProgressBar9.Location = New Point(443, 99)
+        ProgressBar9.Name = "ProgressBar9"
+        ProgressBar9.Size = New Size(125, 29)
+        ProgressBar9.TabIndex = 8
+        ' 
+        ' ProgressBar10
+        ' 
+        ProgressBar10.Location = New Point(443, 329)
+        ProgressBar10.Name = "ProgressBar10"
+        ProgressBar10.Size = New Size(125, 29)
+        ProgressBar10.TabIndex = 9
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(322, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(112, 20)
+        Label1.TabIndex = 20
+        Label1.Text = "ROOM DETAILS"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(36, 169)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(33, 20)
+        Label2.TabIndex = 21
+        Label2.Text = "102"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(36, 229)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(33, 20)
+        Label3.TabIndex = 22
+        Label3.Text = "103"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(36, 280)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(33, 20)
+        Label4.TabIndex = 23
+        Label4.Text = "104"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(36, 343)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(33, 20)
+        Label5.TabIndex = 24
+        Label5.Text = "105"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(390, 106)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(33, 20)
+        Label6.TabIndex = 25
+        Label6.Text = "201"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(390, 171)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(33, 20)
+        Label7.TabIndex = 26
+        Label7.Text = "202"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(390, 229)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(33, 20)
+        Label8.TabIndex = 27
+        Label8.Text = "203"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(390, 280)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(33, 20)
+        Label9.TabIndex = 28
+        Label9.Text = "204"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(390, 336)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(33, 20)
+        Label10.TabIndex = 29
+        Label10.Text = "205"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(507, 66)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(28, 20)
+        Label11.TabIndex = 30
+        Label11.Text = "AC"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(138, 66)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(65, 20)
+        Label12.TabIndex = 31
+        Label12.Text = "NON AC"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(36, 113)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(33, 20)
+        Label13.TabIndex = 32
+        Label13.Text = "101"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(8, 66)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(78, 20)
+        Label14.TabIndex = 33
+        Label14.Text = "ROOM.NO"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(373, 74)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(78, 20)
+        Label15.TabIndex = 34
+        Label15.Text = "ROOM.NO"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(236, 109)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(66, 24)
+        CheckBox1.TabIndex = 35
+        CheckBox1.Text = "LOCK"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(236, 162)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(66, 24)
+        CheckBox2.TabIndex = 36
+        CheckBox2.Text = "LOCK"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(236, 220)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(66, 24)
+        CheckBox3.TabIndex = 37
+        CheckBox3.Text = "LOCK"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(236, 280)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(66, 24)
+        CheckBox4.TabIndex = 38
+        CheckBox4.Text = "LOCK"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(236, 343)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(66, 24)
+        CheckBox5.TabIndex = 39
+        CheckBox5.Text = "LOCK"
+        CheckBox5.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox6
+        ' 
+        CheckBox6.AutoSize = True
+        CheckBox6.Location = New Point(583, 99)
+        CheckBox6.Name = "CheckBox6"
+        CheckBox6.Size = New Size(66, 24)
+        CheckBox6.TabIndex = 40
+        CheckBox6.Text = "LOCK"
+        CheckBox6.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox7
+        ' 
+        CheckBox7.AutoSize = True
+        CheckBox7.Location = New Point(583, 171)
+        CheckBox7.Name = "CheckBox7"
+        CheckBox7.Size = New Size(66, 24)
+        CheckBox7.TabIndex = 41
+        CheckBox7.Text = "LOCK"
+        CheckBox7.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox8
+        ' 
+        CheckBox8.AutoSize = True
+        CheckBox8.Location = New Point(583, 225)
+        CheckBox8.Name = "CheckBox8"
+        CheckBox8.Size = New Size(66, 24)
+        CheckBox8.TabIndex = 42
+        CheckBox8.Text = "LOCK"
+        CheckBox8.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox9
+        ' 
+        CheckBox9.AutoSize = True
+        CheckBox9.Location = New Point(583, 279)
+        CheckBox9.Name = "CheckBox9"
+        CheckBox9.Size = New Size(66, 24)
+        CheckBox9.TabIndex = 43
+        CheckBox9.Text = "LOCK"
+        CheckBox9.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox10
+        ' 
+        CheckBox10.AutoSize = True
+        CheckBox10.Location = New Point(583, 329)
+        CheckBox10.Name = "CheckBox10"
+        CheckBox10.Size = New Size(66, 24)
+        CheckBox10.TabIndex = 44
+        CheckBox10.Text = "LOCK"
+        CheckBox10.UseVisualStyleBackColor = True
+        ' 
+        ' btnBack
+        ' 
+        btnBack.Location = New Point(609, 409)
+        btnBack.Name = "btnBack"
+        btnBack.Size = New Size(94, 29)
+        btnBack.TabIndex = 45
+        btnBack.Text = "BACK"
+        btnBack.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(474, 409)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 46
+        Button2.Text = "OK"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' adminForm2
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(btnBack)
+        Controls.Add(CheckBox10)
+        Controls.Add(CheckBox9)
+        Controls.Add(CheckBox8)
+        Controls.Add(CheckBox7)
+        Controls.Add(CheckBox6)
+        Controls.Add(CheckBox5)
+        Controls.Add(CheckBox4)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ProgressBar10)
+        Controls.Add(ProgressBar9)
+        Controls.Add(ProgressBar8)
+        Controls.Add(ProgressBar7)
+        Controls.Add(ProgressBar6)
+        Controls.Add(ProgressBar5)
+        Controls.Add(ProgressBar4)
+        Controls.Add(ProgressBar3)
+        Controls.Add(ProgressBar2)
+        Controls.Add(ProgressBar1)
+        Name = "adminForm2"
+        Text = "adminForm2"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents ProgressBar3 As ProgressBar
+    Friend WithEvents ProgressBar4 As ProgressBar
+    Friend WithEvents ProgressBar5 As ProgressBar
+    Friend WithEvents ProgressBar6 As ProgressBar
+    Friend WithEvents ProgressBar7 As ProgressBar
+    Friend WithEvents ProgressBar8 As ProgressBar
+    Friend WithEvents ProgressBar9 As ProgressBar
+    Friend WithEvents ProgressBar10 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents CheckBox9 As CheckBox
+    Friend WithEvents CheckBox10 As CheckBox
+    Friend WithEvents btnBack As Button
+    Friend WithEvents Button2 As Button
 End Class
--- seprojectreal/adminForm2.vb
+++ seprojectreal/adminForm2.vb
@@ -1,3 +1,65 @@
 ﻿Public Class adminForm2
+    Inherits System.Windows.Forms.Form
 
+    ' Define a dictionary to keep track of the locked state of each room
+    Private roomLockStatus As New Dictionary(Of Integer, Boolean)()
+
+    Public Sub New()
+        InitializeComponent()
+
+        ' Initialize the dictionary with the initial lock status of each room (assumed to be unlocked initially)
+        For i As Integer = 1 To 10
+            roomLockStatus.Add(i, False)
+        Next
+    End Sub
+
+    ' Handle CheckBox check events to lock/unlock rooms
+    Private Sub CheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged,
+                                                                                    CheckBox2.CheckedChanged,
+                                                                                    CheckBox3.CheckedChanged,
+                                                                                    CheckBox4.CheckedChanged,
+                                                                                    CheckBox5.CheckedChanged,
+                                                                                    CheckBox6.CheckedChanged,
+                                                                                    CheckBox7.CheckedChanged,
+                                                                                    CheckBox8.CheckedChanged,
+                                                                                    CheckBox9.CheckedChanged,
+                                                                                    CheckBox10.CheckedChanged
+        Dim checkBox As CheckBox = CType(sender, CheckBox)
+        Dim roomNumber As Integer = Integer.Parse(checkBox.Name.Substring("CheckBox".Length))
+
+        If checkBox.Checked Then
+            LockRoom(roomNumber)
+        Else
+            UnlockRoom(roomNumber)
+        End If
+    End Sub
+
+    ' Method to lock a room
+    Private Sub LockRoom(roomNumber As Integer)
+        ' Add logic to lock the room (e.g., update database, set room status)
+        roomLockStatus(roomNumber) = True ' Update the lock status for the room
+        MessageBox.Show("Room " & roomNumber.ToString() & " is now locked.")
+    End Sub
+
+    ' Method to unlock a room
+    Private Sub UnlockRoom(roomNumber As Integer)
+        ' Add logic to unlock the room (e.g., update database, set room status)
+        roomLockStatus(roomNumber) = False ' Update the lock status for the room
+        MessageBox.Show("Room " & roomNumber.ToString() & " is now unlocked.")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        ' Close the current form (adminForm2)
+        Me.Close()
+
+        ' Show the admin form again
+        Dim adminform As New adminform()
+        adminform.Show()
+    End Sub
 End Class
+
